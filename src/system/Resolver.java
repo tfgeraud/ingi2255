0729ebe283ac233b8baa668d4c298276f21b01c8
Simon Busard <sibork@Dexter.(none)>
2008-11-21 17:40:14 +0100

    end of system specifications

--- src/system/Resolver.java
+++ src/system/Resolver.java
@@ -1,7 +1,22 @@
 package system;
 
+/**
+ * This interface represents resolver module that is able to close an incident.
+ * Closing an incident means waiting its resolution (sent by ambulance crew),
+ * demobilize ambulance and modify appropriate informations about the incident.
+ * 
+ * @author Simon Busard
+ */
 public interface Resolver {
 
+	/**
+	 * Close the incident with id incidentInfoId.  The ambulance with ambulanceId
+	 * id is mobilized on this incident.
+	 *
+	 * @pre		incidentInfoId a valid id
+	 * 			ambulanceId a valid id
+	 * @post	incident is closed and ambulance is demobilized
+	 */
 	public boolean closeIncident(int incidentInfoId, int ambulanceId);
 
 }
