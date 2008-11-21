6b95157f32f2b702fd0125bcb1c7253d002f5abc
Erick Lavoie <erick.lavoie@gmail.com>
2008-11-21 17:44:40 +0100

    Oubli pour Observer.java dans le simulateur

--- src/simulator/Observer.java
+++ src/simulator/Observer.java
@@ -16,32 +16,8 @@ public interface Observer {
 	 */
 	public void accept(Event event);
 	
-	/**
-	 * Add an observer to the object
-	 * @Pre observer is valid
-	 * @Post observer is added to the list of observers
-	 */
-	public void attach(Observer observer);
 	
-	/**
-	 * Remove an observer from the object
-	 * @Pre observer is valid
-	 * @Post observer is removed from the list of observers
-	 */
-	public void detach(Observer observer);
 	
-	/**
-	 * Notify every registered observer that an Event has occurred
-	 * @Pre Event is valid
-	 * @Post Every observer had received the event through its accept method
-	 */
-	public void notify(Event event);
 	
-	/**
-	 * Execute a simulation step by processing every events in its queue until the step delimiter is reached
-	 * @Pre A step delimiter is in the event queue
-	 * @Post The step delimiter has been consumed and every events preceding it have been processed
-	 */
-	public void step();
 
 }
