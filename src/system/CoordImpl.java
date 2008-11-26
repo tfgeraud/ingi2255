ebe29925f55d1b23a27c3c3b6473c30714be43af
Simon Busard <sibork@Dexter.(none)>
2008-11-26 12:04:11 +0100

    Coordinates implemented

--- src/system/CoordImpl.java
+++ src/system/CoordImpl.java
@@ -2,4 +2,31 @@ package system;
 
 public class CoordImpl implements Coord {
 
+	private int x = 0;
+	private int y = 0;
+	
+	public CoordImpl(int x,int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String toString() {
+		return "(" + x + "," + y + ")";
+	}
 }
