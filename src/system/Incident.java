03b9653bccbf6ae47b6a3587f0dd1b3003cedb60
Antoine Cailliau <antoine.cailliau@student.uclouvain.be>
2008-11-26 12:48:04 +0100

    Update documentation of incident and incidentimple

--- src/system/Incident.java
+++ src/system/Incident.java
@@ -5,144 +5,244 @@ import events.MobilisationOrder;
 import system.exception.UnknownIncidentException;
 
 /**
- * The Incident interface represent the collection
- * of all the incidents stored in the system.
+ * The Incident interface represent the collection of all the incidents stored
+ * in the system.
  * 
+ * @author Antoine Cailliau <antoine.cailliau@student.uclouvain.be>
+ * @author Simon Busard <simon.busard@student.uclouvain.be>
  */
 public interface Incident {
-	
+
+	/**
+	 * The known status of an incdent
+	 */
 	public static final String KNOWN = "known";
+
+	/**
+	 * The processed status of an incident
+	 */
 	public static final String PROCESSED = "processed";
+
+	/**
+	 * The chosen status of an incident
+	 */
 	public static final String CHOSEN = "chosen";
+
+	/**
+	 * The mobilized status of an incident
+	 */
 	public static final String MOBILIZED = "mobilized";
+
+	/**
+	 * The resolved status of an incident
+	 */
 	public static final String RESOLVED = "resolved";
 
 	/**
-	 * Return the ambulance kind that is needed by the incident 
-	 * incidentInfoId.
+	 * Return the ambulance kind that is needed by the incident incidentInfoId.
 	 * 
 	 * @pre incidentInfoId is the id of the incident
 	 * @post return the kind of the ambulance needed by the incident
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public String getAmbulanceKindNeeded(String incidentInfoId)
-	throws UnknownIncidentException ;
+			throws UnknownIncidentException;
 
 	/**
 	 * Return the mobilization order of the incident
 	 * 
 	 * @pre incidentInfoId is the id of the incident
-	 * @post return the order related to the incident.
+	 * @post return the order related to the incident
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public MobilisationOrder getMobOrder(String incidentInfoId)
-	throws UnknownIncidentException ;
+			throws UnknownIncidentException;
 
 	/**
 	 * Return the demobilizationOrder for the incident incidentInfoId.
 	 * 
 	 * @pre incidentInfoId is the id of the incident
 	 * @post return the order related to the incident
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public DemobilisationOrder getDemobOrder(String incidentInfoId)
-	throws UnknownIncidentException ;
-	
+			throws UnknownIncidentException;
+
 	/**
 	 * Return the age of the victim concerned by the incident
 	 * 
 	 * @pre incidentInfoId is the id of the incident
 	 * @post return the age of the victim
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
-	public int getAge(String incidentInfoId)
-	throws UnknownIncidentException ;
-	
+	public int getAge(String incidentInfoId) throws UnknownIncidentException;
+
 	/**
 	 * Return the pregnancy of the victim of the incident
 	 * 
 	 * @pre incidentInfoId is the id of the incident
-	 * @post return the pregrancy of the victim, true if pregrant, false otherwise.
+	 * @post return the pregrancy of the victim, true if pregrant, false
+	 *       otherwise.
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public boolean getPregnant(String incidentInfoId)
-	throws UnknownIncidentException ;
-	
+			throws UnknownIncidentException;
+
 	/**
-	 * Return the localisation of the incident 
+	 * Return the localisation of the incident
 	 * 
 	 * @pre incidentInfoId is the id of the incident
 	 * @post return the localisation of the incident
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public String getLocalisation(String incidentInfoId)
-	throws UnknownIncidentException ;
-	
+			throws UnknownIncidentException;
+
 	/**
-	 * Return the position (in coordinate) of the incident 
+	 * Return the position (in coordinate) of the incident
 	 * 
 	 * @pre incidentInfoId is the id of the incident
 	 * @post return the position (in coordinate) of the incident
-	 */	
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
+	 */
 	public Coord getPosition(String incidentInfoId)
-	throws UnknownIncidentException ;
-	
+			throws UnknownIncidentException;
+
 	/**
 	 * Get the description of the incident.
 	 * 
 	 * @pre incidentInfoId is the id of the incident
 	 * @post return the description of the incident
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public String getDescription(String incidentInfoId)
-	throws UnknownIncidentException ;
-	
+			throws UnknownIncidentException;
+
 	/**
 	 * Get the ambulanceId chosen for the incident.
 	 * 
 	 * @pre incidentInfoId is the id of the incident
 	 * @post return the ambulance chosen for the incident
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public String getChosenAmbulance(String incidentInfoId)
-	throws UnknownIncidentException ;
-	
+			throws UnknownIncidentException;
+
 	/**
 	 * Get the ambulanceId mobilized for the incident.
 	 * 
 	 * @pre incidentInfoId is the id of the incident
 	 * @post return the ambulance mobilized for the incident
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public String getMobilizedAmbulance(String incidentInfoId)
-	throws UnknownIncidentException;
-	
+			throws UnknownIncidentException;
+
 	/**
 	 * Set the ambulanceId chosen for the incident.
 	 * 
 	 * @pre incidentInfoId is the id of the incident
-	 * @post the ambulance ambulanceId is now chosen for the incident incidentInfoId
+	 * @post the ambulance ambulanceId is now chosen for the incident
+	 *       incidentInfoId
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @param ambulanceId
+	 *            The unique identifier of the ambulance
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public void setChosenAmbulance(String incidentInfoId, String ambulanceId)
-	throws UnknownIncidentException;
-	
+			throws UnknownIncidentException;
+
 	/**
 	 * Set the ambulanceId mobilized for the incident.
 	 * 
 	 * @pre incidentInfoId is the id of the incident
-	 * @post the ambulance ambulanceId is now mobilized for the incident incidentInfoId
+	 * @post the ambulance ambulanceId is now mobilized for the incident
+	 *       incidentInfoId
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @param ambulanceId
+	 *            The unique identifier of the ambulance
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public void setMobilizedAmbulance(String incidentInfoId, String ambulanceId)
-	throws UnknownIncidentException;
-	
+			throws UnknownIncidentException;
+
 	/**
 	 * Set the incident as resolved
 	 * 
 	 * @pre incidentInfoId is the id of the incident
 	 * @post the incident incidentInfoId is now considered as resolved.
+	 * 
+	 * @param incidentInfoId
+	 *            The unique identifier of the incident
+	 * @throws UnknownIncidentException
+	 *             The incident is not knwown by the system.
 	 */
 	public void setAsResolved(String incidentInfoId)
-	throws UnknownIncidentException;
-	
+			throws UnknownIncidentException;
+
 	/**
 	 * Add a new incident in the database
 	 * 
-	 * @pre	age the age of the victim implied in the incident. age >= 0
-	 * 		pregnant == true iff the victim is pregnant
-	 * 		localisation the localisation of the incident
-	 * 		description the description of the incident
+	 * @pre age the age of the victim implied in the incident. age >= 0 pregnant ==
+	 *      true iff the victim is pregnant localisation the localisation of the
+	 *      incident description the description of the incident
 	 * @post a new incident is stored in the database
+	 * 
+	 * @param age
+	 *            The age of the victim
+	 * @param pregnant
+	 *            True if the victim is pregnant, false otherwise
+	 * @param localisation
+	 *            The localisation of the incident
+	 * @param description
+	 *            The description of the incident
 	 */
-	public void addIncident(int age, boolean pregnant, String localisation,String description);
+	public void addIncident(int age, boolean pregnant, String localisation,
+			String description);
 }
--- src/system/IncidentImpl.java
+++ src/system/IncidentImpl.java
@@ -8,169 +8,323 @@ import events.MobilisationOrder;
 import system.exception.UnknownIncidentException;
 import system.Ambulance;
 
+/**
+ * This class represent the list of all incident
+ * 
+ * @author Simon Busard <simon.busard@student.uclouvain.be>
+ * @author Antoine Cailliau <antoine.cailliau@student.uclouvain.be>
+ */
 public class IncidentImpl implements Incident {
-	
+
+	/**
+	 * This class represent all the information stored by
+	 * the system about the incidents
+	 * 
+	 * @author Simon Busard <simon.busard@student.uclouvain.be>
+	 * @author Antoine Cailliau <antoine.cailliau@student.uclouvain.be>
+	 */
 	private class IncidentInfo {
+		/**
+		 * The age of the victim. By default, 0.
+		 */
 		int age = 0;
+
+		/**
+		 * The pregnancy of the victim
+		 */
 		boolean pregnant = false;
+
+		/**
+		 * The localisation of the incident
+		 */
 		String localisation = null;
+
+		/**
+		 * The description of the incident
+		 */
 		String description = null;
-		
+
+		/**
+		 * The position of the incident. Computed when 
+		 * we set the localisation of the incident
+		 */
 		Coord position = null;
+
+		/**
+		 * The kind of the ambulance needed for the incident.
+		 * Computed when we set the description of the incident
+		 */
 		String ambKindNeeded = null;
-		
+
+		/**
+		 * The unique identifier of the ambulance chosen for this incident
+		 */
 		String chosenAmb = null;
+
+		/**
+		 * The unique identifier of the ambulance mobilized for this incident
+		 */
 		String mobAmb = null;
-		
+
+		/**
+		 * The state of the incident. Either {@link Incident#KNOWN},
+		 * either {@link Incident#PROCESSED}, either {@link Incident#CHOSEN},
+		 * either {@link Incident#MOBILIZED}, either {@link Incident#RESOLVED}.
+		 */
 		String state = null;
-		
-		public IncidentInfo(int age, boolean preg, String loc, String desc) {
+
+		/**
+		 * Create a new incident
+		 * 
+		 * @param age The age of the victim
+		 * @param pregnant True if the victim is pregrant, false otherwise
+		 * @param localisation The localisation of the incident
+		 * @param description The description of the incident
+		 */
+		public IncidentInfo(int age, boolean pregnant, String localisation, String description) {
 			this.age = age;
-			this.pregnant = preg;
-			this.localisation = loc;
-			this.description = desc;
+			this.pregnant = pregnant;
+			this.localisation = localisation;
+			this.description = description;
 		}
 	}
-	
-	private Hashtable<String, IncidentInfo> incidents = null;
-	private Map map = null;
-	private int nextIncId = 0;
-	
-	// Constructors
+
 	/**
-	 * Create a new instance of Incident.  It represents an empty list of
-	 * known incidents.
+	 * The list of all incident
+	 */
+	private Hashtable<String, IncidentInfo> incidents = null;
+
+	/**
+	 * The map of the system
+	 */
+	private Map map = null;
+
+	/**
+	 * The counter of incident, this number is the next number
+	 * to use (so, not yet used)
+	 */
+	private int nextIncidentId = 0;
+
+	/**
+	 * Create a new instance of Incident. It represents an empty list of known
+	 * incidents.
 	 * 
-	 * @param map : the map contained in the system
+	 * @param map
+	 *            the map contained in the system
 	 */
 	public IncidentImpl(Map map) {
 		this.incidents = new Hashtable<String, IncidentInfo>();
 		this.map = map;
 	}
-		
-	public void addIncident(int age, boolean pregnant, 
-							String localisation, String description) {
-		IncidentInfo inc = new IncidentInfo(age,pregnant,
-											localisation,description);
-		incidents.put("incident" + nextIncId,inc);
-		
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#addIncident(int, boolean, java.lang.String,
+	 *      java.lang.String)
+	 */
+	public void addIncident(int age, boolean pregnant, String localisation,
+			String description) {
+		IncidentInfo inc = new IncidentInfo(age, pregnant, localisation,
+				description);
+		incidents.put("incident" + nextIncidentId, inc);
+
 		// Process additional information
 		// Position
 		inc.position = map.addressToCoord(localisation);
 		// Ambulance kind needed
-		// This is very simple for the beginning.  If description is "grave"
+		// This is very simple for the beginning. If description is "grave"
 		// then a medicalized ambulance is needed, else a normal ambulance
 		// is needed.
 		// TODO : implement more complex and usefull process
-		if(description.equals("grave")) 
+		if (description.equals("grave"))
 			inc.ambKindNeeded = Ambulance.MEDICALIZED;
 		else
 			inc.ambKindNeeded = Ambulance.NORMAL;
 	}
 
-	public int getAge(String incidentInfoId)
-	throws UnknownIncidentException {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getAge(java.lang.String)
+	 */
+	public int getAge(String incidentInfoId) throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		return inc.age;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getAmbulanceKindNeeded(java.lang.String)
+	 */
 	public String getAmbulanceKindNeeded(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		return inc.ambKindNeeded;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getChosenAmbulance(java.lang.String)
+	 */
 	public String getChosenAmbulance(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		return inc.chosenAmb;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getDemobOrder(java.lang.String)
+	 */
 	public DemobilisationOrder getDemobOrder(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
-		return new DemobilisationOrder(	incidentInfoId,
-										inc.position.toString(),inc.mobAmb);
+		if (inc == null)
+			throw new UnknownIncidentException();
+		return new DemobilisationOrder(incidentInfoId, inc.position.toString(),
+				inc.mobAmb);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getDescription(java.lang.String)
+	 */
 	public String getDescription(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		return inc.description;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getLocalisation(java.lang.String)
+	 */
 	public String getLocalisation(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		return inc.localisation;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getMobOrder(java.lang.String)
+	 */
 	public MobilisationOrder getMobOrder(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
-		return new MobilisationOrder(incidentInfoId,
-									 inc.position.toString(),inc.chosenAmb);
+		if (inc == null)
+			throw new UnknownIncidentException();
+		return new MobilisationOrder(incidentInfoId, inc.position.toString(),
+				inc.chosenAmb);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getMobilizedAmbulance(java.lang.String)
+	 */
 	public String getMobilizedAmbulance(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		return inc.mobAmb;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getPosition(java.lang.String)
+	 */
 	public Coord getPosition(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		return inc.position;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#getPregnant(java.lang.String)
+	 */
 	public boolean getPregnant(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		return inc.pregnant;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#setAsResolved(java.lang.String)
+	 */
 	public void setAsResolved(String incidentInfoId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		inc.state = RESOLVED;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#setChosenAmbulance(java.lang.String,
+	 *      java.lang.String)
+	 */
 	public void setChosenAmbulance(String incidentInfoId, String ambulanceId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		inc.chosenAmb = ambulanceId;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see system.Incident#setMobilizedAmbulance(java.lang.String,
+	 *      java.lang.String)
+	 */
 	public void setMobilizedAmbulance(String incidentInfoId, String ambulanceId)
-	throws UnknownIncidentException {
+			throws UnknownIncidentException {
 		IncidentInfo inc = incidents.get(incidentInfoId);
 		// Exception if not known
-		if(inc == null) throw new UnknownIncidentException();
+		if (inc == null)
+			throw new UnknownIncidentException();
 		inc.mobAmb = ambulanceId;
 	}
 }
