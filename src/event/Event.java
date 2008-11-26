e8f57d7bfeb67abb576efce2600279da8be3d720
Antoine Cailliau <antoine.cailliau@student.uclouvain.be>
2008-11-26 09:19:41 +0100

    Update

--- src/event/Event.java
+++ src/event/Event.java
@@ -1,5 +1,13 @@
-package event;
+package system.event;
 
+/**
+ * The Event interface represents all events shared between real world
+ * and software system via communication channels.
+ * List of different events is in report about software architecture.
+ * 
+ * @author Simon Busard
+ *
+ */
 public interface Event {
 
 }
