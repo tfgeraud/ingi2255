d34af211a1a55aa54e8dc7f6f9c199d3c31b8adc
Antoine Cailliau <antoine.cailliau@student.uclouvain.be>
2008-12-02 15:13:35 +0100

    Implement AVLS and MDT. Clean up

--- src/communication/AVLSImpl.java
+++ src/communication/AVLSImpl.java
@@ -5,32 +5,32 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import events.Event;
 
 /**
- * This class represent an AVLS. See {@link AVLS} for more details. 
+ * This class represent an AVLS. See {@link AVLS} for more details.
  * 
  * @author Antoine Cailliau <antoine.cailliau@student.uclouvain.be>
  */
 public class AVLSImpl implements AVLS {
 
 	/**
-	 * This list is used to store event
-	 * that should be delivered to the system
+	 * This list is used to store event that should be delivered to the system
 	 */
 	ConcurrentLinkedQueue<Event> inList;
-	
+
 	/**
 	 * Create a new AVLS, usable by the system
 	 */
 	public AVLSImpl() {
-		inList = new ConcurrentLinkedQueue<Event>();
+		this.inList = new ConcurrentLinkedQueue<Event>();
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see communication.Communication#receive()
 	 */
 	public Event receive() {
-		if (inList.size() >= 0) {
-			return inList.poll();
+		if (this.inList.size() >= 0) {
+			return this.inList.poll();
 		} else {
 			return null;
 		}
@@ -38,10 +38,11 @@ public class AVLSImpl implements AVLS {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see communication.Communication#send(events.Event)
 	 */
 	public void send(Event event) {
 		throw new Error("Unable to send a message through AVLS");
 	}
-	
+
 }
--- src/communication/MDTImpl.java
+++ src/communication/MDTImpl.java
@@ -6,38 +6,38 @@ import events.Event;
 
 /**
  * This class represent a MDT. See {@link MDT} for more details.
- *
+ * 
  * @author Antoine Cailliau <antoine.cailliau@student.uclouvain.be>
  */
 public class MDTImpl implements MDT {
-	
+
 	/**
-	 * This list is used to store event
-	 * that should be delivered to the system
+	 * This list is used to store event that should be delivered to the system
 	 */
 	ConcurrentLinkedQueue<Event> inList;
-	
+
 	/**
-	 * This list is used to store event
-	 * that should be delivered to the external stub
+	 * This list is used to store event that should be delivered to the external
+	 * stub
 	 */
 	ConcurrentLinkedQueue<Event> outList;
-	
+
 	/**
 	 * Create a new MDT, usable by the system
 	 */
 	public MDTImpl() {
-		inList = new ConcurrentLinkedQueue<Event>();
-		outList = new ConcurrentLinkedQueue<Event>();
+		this.inList = new ConcurrentLinkedQueue<Event>();
+		this.outList = new ConcurrentLinkedQueue<Event>();
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see communication.Communication#receive()
 	 */
 	public Event receive() {
-		if (inList.size() >= 0) {
-			return inList.poll();
+		if (this.inList.size() >= 0) {
+			return this.inList.poll();
 		} else {
 			return null;
 		}
@@ -45,11 +45,11 @@ public class MDTImpl implements MDT {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see communication.Communication#send(events.Event)
 	 */
 	public void send(Event event) {
-		outList.add(event);
+		this.outList.add(event);
 	}
-	
 
 }
