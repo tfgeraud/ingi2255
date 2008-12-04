eec89515af46973d58df9bf342e94e88b4bd933c
Erick Lavoie <erick.lavoie@gmail.com>
2008-12-04 22:24:01 +0100

    Created the position object for the map

--- src/simulator/Pos.java
+++ src/simulator/Pos.java
@@ -1,10 +1,29 @@
 package simulator;
 
 /**
- * Provide a position in cartesian coordinates corresponding to a position on the map
+ * Provide a position in cartesian coordinates corresponding to a position on the map.
+ * The position is considered immutable so no setters are provided.
+ * 
  * @author Erick Lavoie
  *
  */
-public interface Pos {
-
+public class Pos {
+	private int x;
+	private int y;
+	
+	public Pos(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	
+	/**
+	 *  Getters and setters 
+	 */
+	int getX() {
+		return this.x;
+	}
+	int getY() {
+		return this.y;
+	}
 }
