808b49bed95a364982045565b6503dfaaeebd94a
Simon Busard <sibork@Dexter.(none)>
2008-12-05 16:34:40 +0100

    Format BrokerImpl.java

--- src/system/BrokerImpl.java
+++ src/system/BrokerImpl.java
@@ -1,30 +1,33 @@
 package system;
 
 /**
- * This class represents the broker.  It stays at the interface between 
- * call center (infoGUI or CallSimul).
+ * This class represents the broker. It stays at the interface between call
+ * center (infoGUI or CallSimul).
  * 
  * @author Simon Busard <simon.busard@student.uclouvain.be>
  */
 public class BrokerImpl implements Broker {
-	
+
 	/**
 	 * The list of incidents
 	 */
 	private Incident incidentList = null;
-	
+
 	public BrokerImpl(Incident incidentList) {
 		this.incidentList = incidentList;
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see system.Broker#addIncidentInfo(java.lang.String, java.lang.String, int, boolean)
+	 * 
+	 * @see system.Broker#addIncidentInfo(java.lang.String, java.lang.String,
+	 *      int, boolean)
 	 */
 	public void addIncidentInfo(String description, String localisation,
 			int victimAge, boolean victimPregnant) {
-		this.incidentList.addIncident(victimAge, victimPregnant, localisation, description);
-		
+		this.incidentList.addIncident(victimAge, victimPregnant, localisation,
+				description);
+
 	}
 
 }
