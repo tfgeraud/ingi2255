a4fc08aa8926201129ba6d2ae2e83f546124555e
Frédéric van der Essen <frederic.vanderessen@student.uclouvain.be>
2008-12-08 14:33:32 +0100

    MapTest

--- src/test/simulator/MapTest.java
+++ src/test/simulator/MapTest.java
@@ -1,9 +1,11 @@
 package test.simulator;
 
+import simulator.*;
+import simulator.simobjects.*;
 import junit.framework.TestCase;
 
 public class MapTest extends TestCase {
-
+	Map map = new Map("square city",9,13);
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
@@ -13,11 +15,20 @@ public class MapTest extends TestCase {
 	}
 
 	public void testNextPos() {
-		fail("Not yet implemented");
+		Pos start = new PosImpl(10,10);
+		Pos end = new PosImpl(10,20);
+		Pos next = map.nextPos(start, end, 5);
+		assertEquals(new PosImpl(10,15),next);
+		next = map.nextPos(start, end, 0);
+		assertEquals(start,next);
+		next = map.nextPos(start, end, 10);
+		assertEquals(end,next);
+		next = map.nextPos(start,end, 15);
+		assertEquals(end,next);
 	}
 
 	public void testAddObstacle() {
-		fail("Not yet implemented");
+		map.addObstacle(new PosImpl(10,10));
 	}
 
 	public void testRemoveObstacle() {
