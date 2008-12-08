c27b2c077435f8a1ee020fde303f2e1bfb5bd935
Erick Lavoie <erick.lavoie@gmail.com>
2008-12-08 14:36:35 +0100

    Ajout de tests pour la deconnexion d un objet de la simulation

---
+++ src/test/simulator/SimObjectImplTest.java
@@ -0,0 +1,23 @@
+package test.simulator;
+
+import junit.framework.TestCase;
+
+public class SimObjectImplTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testDisconnect() {
+		fail("Not yet implemented");
+	}
+
+	public void testObserving() {
+		fail("Not yet implemented");
+	}
+
+}
